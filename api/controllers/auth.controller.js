2298a0a9a62f6fc164f552a8395bbd7a84e90b89
Pratham Rastogi <112170092+Prathamrastogi@users.noreply.github.com>
2024-04-29 16:13:26 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,7 +42,6 @@ export const login = async (req, res) => {
       res
         .cookie("token", token, {
           httpOnly: true,
-          //secure:true,
           maxAge: age,
         })
         .status(200)
